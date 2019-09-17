5eefe75c59ead233613969ab7bee7943b7f4f93d
iLoveOS <923780517@qq.com>
2019-09-17 17:01:06 +0800

    firstPush

---
+++ MagicVideo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MagicVideo
+//
+//  Created by young He on 2019/9/17.
+//  Copyright © 2019年 youngHe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
